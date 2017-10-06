1dc8bf7fcbff5dcfeacbc5d047a7f90857ecc67f
Kelly Stoops <kstoops815@gmail.com>
2017-10-06 13:38:33 -0500

    completed exercise

---
+++ javascripts/greeterMain.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const german = require("./german");
+const yiddish = require("./yiddish");
+const hillbilly = require("./hillbilly");
+
+const greetings = {
+	german, yiddish, hillbilly
+};
+
+module.exports = greetings;
